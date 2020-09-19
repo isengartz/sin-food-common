e6e34910ee67369d01e76cea04b27ae3e3717b24
isengartz <kontokostas.thanasis@gmail.com>
2020-09-20 01:45:04 +0300

    Added Mongoose Duplicate Error

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { CustomError } from "../errors/custom-error";
 import { Error as MongooseError } from "mongoose";
+import { MongoError } from "mongodb";
 
 export const errorHandler = (
   err: Error,
@@ -21,6 +22,16 @@ export const errorHandler = (
     return res
       .status(400)
       .send({ errors: [{ message: `Invalid ${err.path} : ${err.value}` }] });
+  }
+  if (err instanceof MongoError) {
+    if (err.code === 11000) {
+      // @ts-ignore
+      const value = err.message.match(/(["'])(?:(?=(\\?))\2.)*?\1/)[0] || null;
+      const message = `Duplicate field value: ${value} Please use another value!`;
+      return res.status(400).send({
+        errors: [{ message }],
+      });
+    }
   }
   console.error(err);
   res.status(500).send({
