0458cd97c89305e3df6fa8ffce7e65c1509ec954
isengartz <kontokostas.thanasis@gmail.com>
2020-09-20 01:33:45 +0300

    Added Mongoose Errors

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,5 +1,6 @@
-import { Request, Response, NextFunction } from 'express';
-import { CustomError } from '../errors/custom-error';
+import { Request, Response, NextFunction } from "express";
+import { CustomError } from "../errors/custom-error";
+import { Error as MongooseError } from "mongoose";
 
 export const errorHandler = (
   err: Error,
@@ -10,12 +11,22 @@ export const errorHandler = (
   if (err instanceof CustomError) {
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
-
+  if (err instanceof MongooseError.ValidationError) {
+    const mongooseErrors = Object.values(err.errors).map((el) => {
+      return { message: el.message };
+    });
+    return res.status(400).send({ errors: mongooseErrors });
+  }
+  if (err instanceof MongooseError.CastError) {
+    return res
+      .status(400)
+      .send({ errors: [{ message: `Invalid ${err.path} : ${err.value}` }] });
+  }
   console.error(err);
   res.status(500).send({
     errors: [
       {
-        message: 'Something Went Wrong !',
+        message: "Something Went Wrong !",
       },
     ],
   });
