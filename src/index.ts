de982bb5d3d2baf7ac917881f3c39d58a8713c42
isengartz <kontokostas.thanasis@gmail.com>
2020-09-30 19:13:27 +0300

    Added Email Sending Event

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ export * from "./middlewares/validate-request";
 
 export * from "./events/event-interfaces/user-created-event";
 export * from "./events/event-interfaces/user-updated-event";
+export * from "./events/event-interfaces/email-sending-event";
 
 export * from "./events/base-listener";
 export * from "./events/base-publisher";
