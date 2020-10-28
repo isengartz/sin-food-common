c42b640a9bbe4a2b029c6c3b84e67447ca2e391d
isengartz <kontokostas.thanasis@gmail.com>
2020-10-28 21:29:43 +0200

    changes at require-auth-restaurant

--- src/middlewares/require-auth-restaurant.ts
+++ src/middlewares/require-auth-restaurant.ts
@@ -1,11 +1,18 @@
 import { Request, Response, NextFunction } from "express";
-import { NotAuthorizedError } from "..";
+import { NotAuthorizedError, UserRole } from "..";
 
 export const requireAuthRestaurant = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
+  if (
+    req.currentUser &&
+    req.currentUser.role &&
+    req.currentUser.role === UserRole.Admin
+  ) {
+    return next();
+  }
   if (!req.currentRestaurant) {
     throw new NotAuthorizedError();
   }
