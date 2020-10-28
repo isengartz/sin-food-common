7e7050ed8051b367a27fe12c17e2757d23b6a364
isengartz <kontokostas.thanasis@gmail.com>
2020-10-28 19:31:56 +0200

    isAdmin Middleware

--- src/middlewares/is-admin.ts
+++ src/middlewares/is-admin.ts
@@ -3,11 +3,9 @@ import { NotAuthorizedError, UserRole } from "..";
 
 // For routes that require userId param instead of checking if currentUser Id equal userId Param
 // Add this middleware so admin can edit anything but simple user only his collection
-export const isAdmin = (param: string) => {
-  return (req: Request, res: Response, next: NextFunction) => {
-    if (req.currentUser!.role !== UserRole.Admin) {
-      throw new NotAuthorizedError(`You dont have access in this route`);
-    }
-    next();
-  };
+export const isAdmin = (req: Request, res: Response, next: NextFunction) => {
+  if (!req.currentUser || req.currentUser!.role !== UserRole.Admin) {
+    throw new NotAuthorizedError(`You dont have access in this route`);
+  }
+  next();
 };
