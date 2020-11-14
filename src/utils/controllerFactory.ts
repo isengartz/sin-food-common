714c216a9800b43b7fa1250fd28f95da96ee3bfb
isengartz <kontokostas.thanasis@gmail.com>
2020-11-14 21:39:10 +0200

    changed the ControllerFactory

--- src/utils/controllerFactory.ts
+++ src/utils/controllerFactory.ts
@@ -3,16 +3,18 @@ import _ from 'lodash';
 import mongoose from 'mongoose';
 import { NotFoundError } from '../errors/not-found-error';
 import { QueryModelHelper } from './QueryModelHelper';
+import { NotAuthorizedError, UserRole } from '..';
 
 // Create a new document of given Model
-export const createOne = <U extends mongoose.Document, T extends mongoose.Model<U>>(Model: T) => async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
+export const createOne = <
+  U extends mongoose.Document,
+  T extends mongoose.Model<U>
+>(
+  Model: T,
+) => async (req: Request, res: Response, next: NextFunction) => {
   const documentName = Model.collection.collectionName;
   // @ts-ignore
-  const document : U = await Model.build(req.body).save();
+  const document: U = await Model.build(req.body).save();
   res.status(201).json({
     status: 'success',
     data: {
@@ -22,18 +24,30 @@ export const createOne = <U extends mongoose.Document, T extends mongoose.Model<
 };
 
 // Find one document of given Model
-export const findOne = <U extends mongoose.Document, T extends mongoose.Model<U>>(Model: T, populateOptions: {}) => async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
+export const findOne = <
+  U extends mongoose.Document,
+  T extends mongoose.Model<U>
+>(
+  Model: T,
+  populateOptions: {},
+  authField?: string,
+) => async (req: Request, res: Response, next: NextFunction) => {
   const documentName = Model.collection.collectionName;
   // Populate extra data if needed
   let query = Model.findById(req.params.id);
   if (!_.isEmpty(populateOptions)) {
-    query = query.populate( populateOptions );
+    query = query.populate(populateOptions);
   }
   const document = await query;
+  if (authField) {
+    if (
+      // @ts-ignore
+      document[authField] !== req.currentUser!.id &&
+      req.currentUser!.role !== UserRole.Admin
+    ) {
+      throw new NotAuthorizedError('You dont have access to this Document');
+    }
+  }
   res.status(200).json({
     status: 'success',
     data: {
@@ -43,11 +57,13 @@ export const findOne = <U extends mongoose.Document, T extends mongoose.Model<U>
 };
 
 // Find all documents of given Model
-export const findAll = <U extends mongoose.Document, T extends mongoose.Model<U>>(Model: T, populateOptions: {}) => async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
+export const findAll = <
+  U extends mongoose.Document,
+  T extends mongoose.Model<U>
+>(
+  Model: T,
+  populateOptions: {},
+) => async (req: Request, res: Response, next: NextFunction) => {
   const documentName = Model.collection.collectionName;
 
   const queryHelper = new QueryModelHelper(Model.find(), req.query)
@@ -75,16 +91,31 @@ export const findAll = <U extends mongoose.Document, T extends mongoose.Model<U>
 };
 
 // Delete a document of given Model
-export const deleteOne = <U extends mongoose.Document, T extends mongoose.Model<U>>(Model: T) => async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
+export const deleteOne = <
+  U extends mongoose.Document,
+  T extends mongoose.Model<U>
+>(
+  Model: T,
+  authField?: string,
+) => async (req: Request, res: Response, next: NextFunction) => {
   const documentName = Model.collection.collectionName;
-  const document = await Model.findByIdAndDelete(req.params.id);
+  const document = await Model.findById(req.params.id);
   if (!document) {
     throw new NotFoundError(`No ${documentName} found with that ID`);
   }
+  // if we define an authField check if current user own the Document
+  if (authField) {
+    if (
+      // @ts-ignore
+      document[authField] !== req.currentUser!.id &&
+      req.currentUser!.role !== UserRole.Admin
+    ) {
+      throw new NotAuthorizedError('You dont have access to this Document');
+    }
+  }
+
+  await document.remove();
+
   res.status(204).json({
     status: 'success',
     data: null,
@@ -92,23 +123,40 @@ export const deleteOne = <U extends mongoose.Document, T extends mongoose.Model<
 };
 
 // Updates a document of given Model
-export const updateOne = <U extends mongoose.Document, T extends mongoose.Model<U>>(Model: T) => async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
+export const updateOne = <
+  U extends mongoose.Document,
+  T extends mongoose.Model<U>
+>(
+  Model: T,
+  authField?: string,
+) => async (req: Request, res: Response, next: NextFunction) => {
   const documentName = Model.collection.collectionName;
-  const document = await Model.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true,
-  });
+  const document = await Model.findById(req.params.id);
+  // AndUpdate(req.params.id, req.body, {
+  //   new: true,
+  //   runValidators: true,
+  // }
   if (!document) {
     throw new NotFoundError(`No ${documentName} found with that ID`);
   }
+  // if we define an authField check if current user own the Document
+  if (authField) {
+    if (
+      // @ts-ignore
+      document[authField] !== req.currentUser!.id &&
+      req.currentUser!.role !== UserRole.Admin
+    ) {
+      throw new NotAuthorizedError('You dont have access to this Document');
+    }
+  }
+  const updatedDocument = await document.update(req.body, {
+    new: true,
+    runValidators: true,
+  });
   res.status(200).json({
     status: 'success',
     data: {
-      [documentName]: document,
+      [documentName]: updatedDocument,
     },
   });
 };
