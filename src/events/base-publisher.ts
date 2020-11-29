38a276ddd0cb65c9d9cd7798db34b201678bb595
isengartz <kontokostas.thanasis@gmail.com>
2020-11-29 18:30:22 +0200

    added menu event interfaces

--- src/events/base-publisher.ts
+++ src/events/base-publisher.ts
@@ -20,6 +20,7 @@ export abstract class Publisher<T extends Event> {
         if (err) {
           return reject(err);
         }
+        console.log(`Event Published: ${this.subject}`);
         resolve();
       });
     });
