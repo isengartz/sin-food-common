d75f9e2f8e352184e67c68d9f4082d857f673efd
isengartz <kontokostas.thanasis@gmail.com>
2021-04-24 18:42:23 +0300

    Little fix in Password Class

--- src/utils/password.ts
+++ src/utils/password.ts
@@ -2,17 +2,15 @@ import { scrypt, randomBytes } from 'crypto';
 import { promisify } from 'util';
 
 export class Password {
-  static scryptAsync = promisify(scrypt);
-
   static async toHash(password: string) {
     const salt = randomBytes(8).toString('hex');
-    const buffer = (await this.scryptAsync(password, salt, 64)) as Buffer;
+    const buffer = (await promisify(scrypt)(password, salt, 64)) as Buffer;
     return `${buffer.toString('hex')}.${salt}`;
   }
 
   static async compare(storedPassword: string, suppliedPassword: string) {
     const [hashedPassword, salt] = storedPassword.split('.');
-    const buffer = (await this.scryptAsync(
+    const buffer = (await promisify(scrypt)(
       suppliedPassword,
       salt,
       64,
